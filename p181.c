bde256f0f085a63aede6bb47b64074d21e1b707b
Buttu1 <46070595+Buttu1@users.noreply.github.com>
2019-04-03 15:35:30 +0530

    Create p181.c

---
+++ p181.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main() {
+   int a=3,b=7,i,j,n;
+   scanf("%d",&n);
+   if(n%a==0)
+   {
+       printf("yes");
+   }
+   else if(n%b==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+   }
